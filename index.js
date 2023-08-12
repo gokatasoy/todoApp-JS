feed5b2e638ca6423c6b4f4c85996b6bf9cd164f
Gökberk ATASOY <gokatasoycs@gmail.com>
2023-08-12 19:44:42 +0300

    Add html and css styles

---
+++ index.js
@@ -0,0 +1,16 @@
+// TODO APP
+// 1-) Start
+// 2-) Add Html Structure and Css
+// 3-) Close butonuna ulaş ve bi fonksiyon yaz. Bu fonksiyona tıkladığın zaman (onclick) parent elementinin styleine ulaşıp display none yapacaksın
+// 4-) ul elemanlarına ulaş ve bu elemana bi event listener ekle. Bu event listener sayesinde elemente tıkladığın zaman checed classı vereceksin
+// 5-) Add butonuna tıkladığın zaman inputtaki eleman listeye eklenecek.
+// 6-) newElement diye bi fonksiyon çağır. 
+// 7-) Li adında bi etiket oluştur ve değişken içine at
+// 8-) input değerini al ve dğişken içine ata.
+// 9-) input değişkenini innerHtml içine al ve yine bi değişken içine at
+// 10-) li.appendChild metodunu kullan. Burada inputun değeri üzerinden kontrol işlemi yap dolu veya boş gibisinden.
+// 11-) Bu işlemden sonra input değişkenine ulaş ve input içini sıfırla.
+// 12-) Listeden çıkarma fonksiyonu
+// 13-) 
+
+// close => display:none, onclick => add checked class, add => addNewItem
