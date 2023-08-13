a0d5cad5262f22146c3fec4fd2773bb833bda7dc
Gökberk ATASOY <gokatasoycs@gmail.com>
2023-08-13 21:33:43 +0300

    Add bootstrap classes to dom

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 // TODO APP
+
 // 1-) Start
-// 2-) Add butonuna ulaş değişken içine at
-// 3-) inputa ulaş bi değişken içine at
-// 4-) appendChild kullanıp içine li elemanı eklemek için ul elemanına ulaş ve bi değişken içine ata.
-// 5-) Add butonuna click eventi ekle
-// 6-) Add butonuna bastığında çağrılacak fonksiyonu yaz => addTask
-// 7-) addTask fonksiyonunda inputun value değeri üzerinden boş mu kontrolünü yap
-// 8-) else durumunda ul'ye li ekleyecek fonksiyonu ekleyeceğiz => renderTodoItem ve buna input'un value değerini parametre olarak geçerceğiz => todoText
-// 9-) Bu fonksiyon todoText tiye bi parametre alacak.
-// 10-) li elementi create et ve bunu todoItemElement adındaki değişkene ata.
-// 11-) Bu elemente ulaş ve classlistine todo_item ekle
-// 12-) completeButton oluştur createElement yardımıyla
-// 13-) bu elemente gerekli classı ekle classList yardımıyla
-// 14-) butonun gerekli innerTextini ekle
-// 15-) ve li elementine bu buttonu append et
-// 16-) aynısını delete button için de yap
-// 17-) textElement diye bi değişken oluştur ve p etiketi create et
-// 18-) Bu elementin innerTextini parametreden gelen değeri aktar
-// 19-) ve bu textElementi ul içine append et
-// 20-) Ve bu elementi ul'ye append et.
+// 2-) Access the "Add" button, assign to a variable named addBtn
+// 3-) Access the input element, assign to a variable named todoInput
+// 4-) Access the task list element, assign to a variable named todoList
+// 5-) Add a click event listener to addBtn, calling the function addTask
+// 6-) Create a function named renderTodoItem that takes todoText as a parameter:
+//    - Add "list-group" and "list-group-flush" classes to todoList
+//    - Create a "li" element, assign to listItem
+//    - Add classes "list-group-item", "d-flex", "justify-content-between", and "align-items-center" to listItem
+//    - Create a "span" element, assign to buttonContainer
+//    - Create a "p" element, assign to textElement
+//    - Set textElement's inner text to todoText
+//    - Append textElement to listItem
+//    - Create a "button" element, assign to completeButton
+//    - Add classes "btn", "btn-success", "btn-sm", and "me-1" to completeButton
+//    - Set completeButton's width to "80px"
+//    - Set completeButton's inner text to "Complete"
+//    - Append completeButton to buttonContainer
+//    - Create a "button" element, assign to deleteButton
+//    - Add classes "btn", "btn-danger", "btn-sm", and "me-1" to deleteButton
+//    - Set deleteButton's width to "80px"
+//    - Set deleteButton's inner text to "Delete"
+//    - Append deleteButton to buttonContainer
+//    - Append buttonContainer to listItem
+//    - Append listItem to todoList
+
+// 7-) Create a function named addTask:
+//    - If todoInput's value is empty:
+//        - Show an alert: "You cannot submit an empty task!"
+//    - Else:
+//        - Call the renderTodoItem function with todoInput's value as a parameter
+
+// 8-) End
+
+
 
 
 // ADDING TASK TO THE LIST
@@ -27,22 +43,35 @@ let todoInput = document.getElementById("todo_input")
 let todoList = document.getElementById("todo_list")
 
 const renderTodoItem = (todoText) => {
+    // UL ELEMENT
+    todoList.classList.add("list-group", "list-group-flush")
+
+    // LIST ELEMENT
     let todoElement = document.createElement("li")
-    todoElement.classList.add("todoItem")
+    todoElement.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center", "justify-content-center")
 
-    let completeButton = document.createElement("button")
-    completeButton.classList.add("complete_button")
-    completeButton.innerText = "Complete"
-    todoElement.appendChild(completeButton)
+    let buttonContainer = document.createElement("span")
 
-    let deleteButton = document.createElement("button")
-    deleteButton.classList.add("delete_button")
-    deleteButton.innerText = "Delete"
-    todoElement.appendChild(deleteButton)
-
+    // TEXT
     let textElement = document.createElement("p")
     textElement.innerText= todoText
     todoElement.appendChild(textElement)
+
+    // COMPLETE BUTTON
+    let completeButton = document.createElement("button")
+    completeButton.classList.add("btn", "btn-success", "btn-sm", "me-1")
+    completeButton.style.width="80px"
+    completeButton.innerText = "Complete"
+    buttonContainer.appendChild(completeButton)
+
+    // DELETE BUTTON
+    let deleteButton = document.createElement("button")
+    deleteButton.classList.add("btn", "btn-danger", "btn-sm", "me-1")
+    deleteButton.style.width="80px"
+    deleteButton.innerText = "Delete"
+    buttonContainer.appendChild(deleteButton)
+
+    todoElement.appendChild(buttonContainer)
 
     todoList.appendChild(todoElement)
 }
